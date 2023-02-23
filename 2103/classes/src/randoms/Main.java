5bd1dcd96c8f34f516713b6415a236187e591833
keylperez <perez.williamcaleb@gmail.com>
2023-02-23 18:25:13 +0800

    organized files

--- 2103/classes/src/randoms/Main.java
+++ 2103/classes/src/randoms/Main.java
@@ -2,22 +2,28 @@ package randoms;
 
 public class Main {
 
-	MyTime t = new MyTime(3, 45, 0, "AM");
+	public static void main(String[] args) {
+		
+		Student stud = new Student(18104944, "William", "BSCS", 3);
+		stud.displayStud();
 
-    t.displayTime();
-    t.tickBySecond();
-    t.displayTime();
-    t.tickByMinute();
-    t.displayTime();
-    t.tickByHour();
-    t.displayTime();
+		MyTime t = new MyTime(3, 45, 0, "AM");
 
-    MyTime t2 = new MyTime(12, 59, 58, "AM");
-    t2.displayTime();
-    t2.tickBySecond();
-    t2.displayTime();
-    t2.tickByMinute();
-    t2.displayTime();
-    t2.tickByHour();
-    t2.displayTime();
+		t.displayTime();
+		t.tickBySecond();
+		t.displayTime();
+		t.tickByMinute();
+		t.displayTime();
+		t.tickByHour();
+		t.displayTime();
+
+		MyTime t2 = new MyTime(12, 59, 58, "AM");
+		t2.displayTime();
+		t2.tickBySecond();
+		t2.displayTime();
+		t2.tickByMinute();
+		t2.displayTime();
+		t2.tickByHour();
+		t2.displayTime();
+	}
 }
--- 2103/classes/src/randoms/Student.java
+++ 2103/classes/src/randoms/Student.java
@@ -1,41 +1,62 @@
 package randoms;
 
 public class Student {
-    public int studID;
-    public String studName;
-    public String program;
-    public int yearLevel;
+	public int studID;
+	public String studName;
+	public String program;
+	public int yearLevel;
 
-    public Student() {
-        this(1000, "Default Name");
-    }
+	public Student() {
+		this(1000, "Default Name", "Default Program", 0);
+	}
 
-    public Student(int studID, String studName) {
-        this.studID = studID;
-        this.studName = studName;
-    }
+	public Student(int studID, String studName, String program, int yearLevel) {
+		this.studID = studID;
+		this.studName = studName;
+		this.program = program;
+		this.yearLevel = yearLevel;
+	}
+	
+	public void setStudID(int studID) {
+		this.studID = studID;
+	}
 
-    public void setSTudID(int studID) {
-        this.studID = studID;
-    }
+	public int getStudID() {
+		return studID;
+	}
 
-    public int getStudID() {
-        return studID;
-    }
+	public String getStudName() {
+		return studName;
+	}
 
-    // public Student(String studName, int studID){
-    // this.studID = studID;
-    // this.studName = studName;
-    // }
+	public void setStudName(String studName) {
+		this.studName = studName;
+	}
 
-    public void shiftToOtherProgram(String program) {
-        this.program = program;
-    }
+	public String getProgram() {
+		return program;
+	}
 
-    public void displayStud() {
-        System.out.println("Student ID: " + studID);
-        System.out.println("Student Name: " + studName);
-        System.out.println("Program: " + program);
-        System.out.println("Year Level: " + yearLevel);
-    }
+	public void setProgram(String program) {
+		this.program = program;
+	}
+
+	public int getYearLevel() {
+		return yearLevel;
+	}
+
+	public void setYearLevel(int yearLevel) {
+		this.yearLevel = yearLevel;
+	}
+
+	public void shiftToOtherProgram(String program) {
+		this.program = program;
+	}
+
+	public void displayStud() {
+		System.out.println("Student ID: " + studID);
+		System.out.println("Student Name: " + studName);
+		System.out.println("Program: " + program);
+		System.out.println("Year Level: " + yearLevel);
+	}
 }
