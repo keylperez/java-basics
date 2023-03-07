52d432884c4054e49f91ac6b592722be73b6467c
keylperez <perez.williamcaleb@gmail.com>
2023-03-07 10:21:55 +0800

    v1

---
+++ industry-assignment/src/Employee.java
@@ -0,0 +1,37 @@
+
+public class Employee {
+	private int empID;
+	private String empName;
+
+	public Employee() {
+		super();
+	}
+
+	public Employee(int empID, String empName) {
+		this.empID = empID;
+		this.empName = empName;
+	}
+
+	public int getEmpID() {
+		return empID;
+	}
+
+	public void setEmpID(int empID) {
+		this.empID = empID;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	public void displayInfo() {
+		System.out.println("Employee");
+		System.out.println("ID: " + this.getEmpID());
+		System.out.println("Name: " + this.getEmpName());
+//			System.out.println(this);
+	}
+}
