b845aa5b8b5a37b8241cd253b8b6428cb3e932b2
keylperez <perez.williamcaleb@gmail.com>
2023-03-09 12:13:33 +0800

    version 2

--- industry-assignment/src/Main.java
+++ industry-assignment/src/Main.java
@@ -4,15 +4,20 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Employee e = new Employee(1000, "John");
-		e.displayInfo();
+
 		HourlyEmployee he = new HourlyEmployee(e.getEmpID(), e.getEmpName(), 100, 8);
-		PieceWorkerEmployee pwe = new PieceWorkerEmployee(e.getEmpID(), e.getEmpName(), 101, 2);
 		System.out.println();
 		he.displayInfo();
+		PieceWorkerEmployee pwe = new PieceWorkerEmployee(e.getEmpID(), e.getEmpName(), 101, 2);
+		System.out.println();
+		pwe.displayInfo();
+
 		CommissionEmployee ce = new CommissionEmployee(e.getEmpID(), e.getEmpName(), 10000);
-		BasePlusCommissionEmployee bpce = new BasePlusCommissionEmployee(e.getEmpID(), e.getEmpName(), pwe.totalPrice(), ce.getTotalSales(), ce.getCommission());
+		BasePlusCommissionEmployee bpce = new BasePlusCommissionEmployee(e.getEmpID(), e.getEmpName(), 200,
+		ce.getTotalSales(), ce.getCommission());
 		System.out.println();
 		bpce.displayInfo();
+
 	}
 
 }
